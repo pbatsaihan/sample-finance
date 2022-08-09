e8c9c717d9520ba9e0613917684fa63b9d95d961
pbatsaihan <batsaihan.p@gmail.com>
2022-08-09 12:40:39 +0800

    Дэлгэцээс утга авдаг болгох

--- app.js
+++ app.js
@@ -1,21 +1,44 @@
 // Дэлгэцтэй ажиллах контролер
-var uiController = (function () {})();
+var uiController = (function () {
+  var DOMstrings = {
+    inputType: ".add__type",
+    inputDescription: ".add__description",
+    inputValue: ".add__value",
+    addBtn: ".add__btn",
+  };
+
+  return {
+    getInput: function () {
+      return {
+        type: document.querySelector(DOMstrings.inputType).value,
+        description: document.querySelector(DOMstrings.inputDescription).value,
+        value: document.querySelector(DOMstrings.inputValue).value,
+      };
+    },
+
+    getDOMstrings: function () {
+      return DOMstrings;
+    },
+  };
+})();
 
 // Санхүүгийн контролер
 var financeController = (function () {})();
 
 // Програм холбогч контролер
 var appController = (function (uiController, financeController) {
+  var DOM = uiController.getDOMstrings();
+
   var ctrlAddItem = function () {
     // 1.Оруулах өгөгдлийг олж авна
+    console.log(uiController.getInput());
     // 2. Олж авсан өгөгдлүүдийг санхүүгийн контролор руу дамжуулж тэнд хадгалана.
     // 3. Олж авсан өгөгдлүүдийг вэб дээр тохирох хэсэгт гаргана
     // 4. Төсвийг тооцоолно.
     // 5. Эцсийн үлдэгдэл тооцоолж дэлгэцэнд гаргана.
-    console.log("Дэлгэцээс өгөгдөл авах хэсэг");
   };
 
-  document.querySelector(".add__btn").addEventListener("click", function () {
+  document.querySelector(DOM.addBtn).addEventListener("click", function () {
     ctrlAddItem();
   });
 
