8821b8400727f269ff23f86489cea030a7b2144c
pbatsaihan <batsaihan.p@gmail.com>
2022-08-12 21:07:27 +0800

    Талбаруудын өнгийг солих

--- app.js
+++ app.js
@@ -63,6 +63,22 @@ var uiController = (function () {
         months[today.getMonth()] + " " + today.getFullYear();
     },
 
+    changeType: function () {
+      var fields = document.querySelectorAll(
+        DOMstrings.inputType +
+          ", " +
+          DOMstrings.inputDescription +
+          ", " +
+          DOMstrings.inputValue
+      );
+
+      nodeListForeach(fields, function (el) {
+        el.classList.toggle("red-focus");
+      });
+
+      document.querySelector(DOMstrings.addBtn).classList.toggle("red");
+    },
+
     getInput: function () {
       return {
         type: document.querySelector(DOMstrings.inputType).value,
@@ -334,6 +350,10 @@ var appController = (function (uiController, financeController) {
         ctrlAddItem();
       }
     });
+
+    document
+      .querySelector(DOM.inputType)
+      .addEventListener("change", uiController.changeType);
 
     document
       .querySelector(DOM.containerDiv)
